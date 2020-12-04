11e8bddf0af8d0a7042ed084f628cf079efc1a81
Paul Lerner <lerner@tuta.io>
2020-12-04 18:18:44 +0100

    chore: add setup.py to allow installation of meerqat package

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='meerqat',
+    packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+
+    author='Paul Lerner',
+    author_email='lerner@limsi.fr',
+    url='https://github.com/PaulLerner/meerqat',
+
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Scientific/Engineering"
+    ],
+)
+
