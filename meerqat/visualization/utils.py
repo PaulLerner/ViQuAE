7749d3e7f537eaf4cafaf90d518c57ca8f2c34c2
Paul Lerner <lerner@tuta.io>
2020-12-22 09:28:28 +0100

    feat: utils: make viz_pos more flexible by allowing different attributes, rename to viz_spacy

--- meerqat/visualization/utils.py
+++ meerqat/visualization/utils.py
@@ -2,16 +2,19 @@
 from tabulate import tabulate
 
 
-def viz_pos(doc):
+def viz_spacy(doc, attrs=["text", "pos_", "dep_", "ent_type_"]):
     """
     Parameters
     ----------
     doc: spacy Doc
+    attrs: List
+        Attributes needed for visualization,
+        defaults to tokens, POS, dependency tag and entity type.
 
     Returns
     -------
     string: str
-        Input document tabulated with a line for the tokens and another for POS
+        Input document tabulated with a line for each doc attributes (tokens, POS, ...)
     """
-    pos = [(token.text, token.pos_) for token in doc]
-    return tabulate(zip(*pos))
+    viz = [[getattr(token, attr) for attr in attrs] for token in doc]
+    return tabulate(zip(*viz))
