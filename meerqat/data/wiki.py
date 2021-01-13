c999bd5b2a4cfc920e55532f4e28cb229df5abd9
Paul Lerner <lerner@tuta.io>
2021-01-13 14:51:38 +0100

    feat: query depictions of entities using beta-commons SPARQL API + refactor wikidata query

--- meerqat/data/wiki.py
+++ meerqat/data/wiki.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 """Usage:
 wiki.py data <subset>
+wiki.py commons sparql <subset>
 """
 import json
 from SPARQLWrapper import SPARQLWrapper, JSON
@@ -27,25 +28,35 @@ SELECT ?entity ?entityLabel ?instanceof ?instanceofLabel ?commons ?image ?occupa
 """
 WIKIDATA_ENDPOINT = "https://query.wikidata.org/sparql"
 
+# template for beta-commons SPARQL API to query images that depict (P180) entities
+# same usage as WIKIDATA_QUERY
+COMMONS_SPARQL_QUERY = """
+SELECT ?depicted_entity ?commons_entity ?special_path ?url WHERE {
+  VALUES ?depicted_entity { %s }
+  ?commons_entity wdt:P180 ?depicted_entity .
+  ?commons_entity schema:contentUrl ?url .
+  bind(iri(concat("http://commons.wikimedia.org/wiki/Special:FilePath/", wikibase:decodeUri(substr(str(?url),53)))) AS ?special_path)
+}
+"""
+COMMONS_SPARQL_ENDPOINT = "https://wcqs-beta.wmflabs.org/sparql"
 
-def data(wikidata_ids):
-    """
-    Query WIKIDATA_QUERY ('instance of', 'commons category', ...) for entities
-    in wikidata_ids
 
-    Parameters
-    ----------
-    wikidata_ids: List[str]
-        List of wikidata QIDs
+def query_sparql_entities(query, endpoint, wikidata_ids,
+                          n=100, return_format=JSON, description=""):
     """
-    sparql = SPARQLWrapper(WIKIDATA_ENDPOINT)
-    sparql.setReturnFormat(JSON)
+    Queries query%entities by batch of n (defaults 100)
+    where entities is n QIDs in wikidata_ids space-separated and prefixed by 'wd:'
+
+    Returns query results
+    """
+    sparql = SPARQLWrapper(endpoint)
+    sparql.setReturnFormat(return_format)
     results, qids = [], []
-    # query only 100 qid at a time
-    for i, qid in enumerate(tqdm(wikidata_ids, desc="Querying Wikidata")):
+    # query only n qid at a time
+    for i, qid in enumerate(tqdm(wikidata_ids, desc=description)):
         qids.append(f"wd:{qid}")
-        if (i+1) % 100 == 0 or i == (len(wikidata_ids) - 1):
-            sparql.setQuery(WIKIDATA_QUERY % " ".join(qids))
+        if (i + 1) % n == 0 or i == (len(wikidata_ids) - 1):
+            sparql.setQuery(query % " ".join(qids))
             results += sparql.query().convert()['results']['bindings']
             qids = []
     print(f"Query succeeded! Got {len(results)} results")
@@ -53,18 +64,12 @@ def data(wikidata_ids):
     return results
 
 
-def update_from_data(subset):
-    """
-    Loads entities from {DATA_ROOT_PATH}/meerqat_{subset}/entities.json and updates them with info
-    queried in 'data' (from wikidata)
-    """
-    # load entities
-    path = DATA_ROOT_PATH / f"meerqat_{subset}" / "entities.json"
-    with open(path) as file:
-        entities = json.load(file)
+def update_from_data(entities):
+    """Updates entities with info queried in from Wikidata"""
 
-    # query wikidata
-    results = data(entities.keys())
+    # query Wikidata
+    results = query_sparql_entities(WIKIDATA_QUERY, WIKIDATA_ENDPOINT, entities.keys(),
+                                    description="Querying Wikidata")
 
     # update entities with results
     for result in tqdm(results, desc="Updating entities"):
@@ -83,15 +88,48 @@ def update_from_data(subset):
             multiple_value = result[multiple_key]['value']
             entities[qid][multiple_key][multiple_value] = result[multiple_key]
 
-    with open(path, 'w') as file:
-        json.dump(entities, file)
+    return entities
 
-    print(f"Successfully saved output to {path}")
+
+def update_from_commons_sparql(entities):
+    # query Wikimedia Commons
+    results = query_sparql_entities(COMMONS_SPARQL_QUERY, COMMONS_SPARQL_ENDPOINT,
+                                    entities.keys(),
+                                    description="Querying Wikimedia Commons")
+
+    # update entities with results
+    for result in tqdm(results, desc="Updating entities"):
+        qid = result['depicted_entity']['value'].split('/')[-1]
+        commons_qid = result['commons_entity']['value']
+        # create a new key 'depictions' to store depictions in a dict
+        entities[qid].setdefault("depictions", {})
+        # use commons_qid (e.g. https://commons.wikimedia.org/entity/M88412327) as key in this dict
+        entities[qid]["depictions"].setdefault(commons_qid, {})
+        entities[qid]["depictions"][commons_qid]['url'] = result['url']
+        entities[qid]["depictions"][commons_qid]['special_path'] = result['special_path']
+
+    return entities
+
 
 if __name__ == '__main__':
     # parse arguments
     args = docopt(__doc__)
     subset = args['<subset>']
 
+    # load entities
+    path = DATA_ROOT_PATH / f"meerqat_{subset}" / "entities.json"
+    with open(path) as file:
+        entities = json.load(file)
+
+    # update from Wikidata or Wikimedia Commons
     if args['data']:
-        update_from_data(subset)
+        entities = update_from_data(entities)
+    elif args['commons']:
+        if args['sparql']:
+            entities = update_from_commons_sparql(entities)
+
+    # save output
+    with open(path, 'w') as file:
+        json.dump(entities, file)
+
+    print(f"Successfully saved output to {path}")
