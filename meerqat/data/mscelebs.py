95d3558d3f9646d3c53f1f2796ba32731dba53f1
Paul Lerner <lerner@tuta.io>
2021-02-02 15:14:00 +0100

    feat: mscelebs extract: extract images from tsv

--- meerqat/data/mscelebs.py
+++ meerqat/data/mscelebs.py
@@ -1,9 +1,12 @@
 # coding: utf-8
 """Usage:
 mscelebs.py entities <subset>
+mscelebs.py extract
 """
 import csv
 import json
+import base64
+import csv
 
 from pathlib import Path
 from tqdm import tqdm
@@ -11,6 +14,8 @@ from docopt import docopt
 
 from meerqat.data.loading import DATA_ROOT_PATH
 
+
+TOTAL_LINES = 8456240
 
 def wikidata2freebase(mid):
     """For some reason the mid format on Wikidata is '/m/0xxx' but on Freebase it's 'm.0xxx'"""
@@ -33,7 +38,7 @@ def count_entities(entities, mscelebs_path):
     fb2wd = freebase2wikidata(entities)
     with open(mscelebs_path) as file:
         reader = csv.reader(file, delimiter='\t')
-        for row in tqdm(reader, desc="Reading MS-Celebs"):
+        for row in tqdm(reader, desc="Reading MS-Celebs", total=TOTAL_LINES):
             mid = row[0]
             qid = fb2wd.get(mid)
             if qid:
@@ -42,19 +47,38 @@ def count_entities(entities, mscelebs_path):
     return entities
 
 
+def extract(mscelebs_path):
+    """Based on https://github.com/cmusatyalab/openface/blob/master/data/ms-celeb-1m/extract.py"""
+    output_dir = mscelebs_path.parent/"jpgs"
+    output_dir.mkdir(exist_ok=True)
+    with open(mscelebs_path) as tsv:
+        reader = csv.reader(tsv, delimiter='\t')
+        for row in tqdm(reader, desc="Extracting images", total=TOTAL_LINES):
+            mid, imgSearchRank, faceID, data = row[0], row[1], row[4], base64.b64decode(row[-1])
+
+            entity_dir = output_dir / mid
+            entity_dir.mkdir(exist_ok=True)
+            output_path = entity_dir / f"{imgSearchRank}-{faceID}.jpg"
+
+            with open(output_path, 'wb') as jpg:
+                jpg.write(data)
+
+
 if __name__ == '__main__':
     # parse arguments
     args = docopt(__doc__)
     subset = args['<subset>']
-    # load data
+
+    mscelebs_path = Path(DATA_ROOT_PATH, "MS-Celeb-1M/data/croped_face_images/FaceImageCroppedWithOutAlignment.tsv")
     subset_path = DATA_ROOT_PATH / f"meerqat_{subset}"
     entities_path = subset_path / "entities.json"
-    mscelebs_path = Path(DATA_ROOT_PATH, "MS-Celeb-1M/data/croped_face_images/FaceImageCroppedWithOutAlignment.tsv")
-    with open(entities_path) as file:
-        entities = json.load(file)
 
-    if args['entities']:
+    if args['extract']:
+        extract(mscelebs_path)
+    elif args['entities']:
+        # load data
+        with open(entities_path) as file:
+            entities = json.load(file)
         entities = count_entities(entities, mscelebs_path)
-
-    with open(entities_path, "w") as file:
-        json.dump(entities, file)
+        with open(entities_path, "w") as file:
+            json.dump(entities, file)
