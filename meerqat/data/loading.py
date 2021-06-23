e4ed6d0d887e78d1b81a1f73fb7ae19842908a32
Paul Lerner <lerner@tuta.io>
2021-06-23 10:25:06 +0200

    fix add original answers in kilt-TriviaQA, see https://github.com/huggingface/datasets/issues/2391

--- meerqat/data/loading.py
+++ meerqat/data/loading.py
@@ -20,9 +20,19 @@ def map_kilt_triviaqa():
     trivia_qa = load_dataset('trivia_qa', 'unfiltered.nocontext')
     # The KILT IDs can then be mapped to the TriviaQA questions with:
     triviaqa_map = {}
+
+    def add_missing_data(x, trivia_qa_subset, triviaqa_map):
+        i = triviaqa_map[x['id']]
+        x['input'] = trivia_qa_subset[i]['question']
+        x['output']['original_answer'] = trivia_qa_subset[i]['answer']['value']
+        return x
+
     for k in ['train', 'validation', 'test']:
         triviaqa_map = dict([(q_id, i) for i, q_id in enumerate(trivia_qa[k]['question_id'])])
         kilt_tasks[k + '_triviaqa'] = kilt_tasks[k + '_triviaqa'].filter(lambda x: x['id'] in triviaqa_map)
-        kilt_tasks[k + '_triviaqa'] = kilt_tasks[k + '_triviaqa'].map(lambda x: {'input': trivia_qa[k][triviaqa_map[x['id']]]['question']})
+        kilt_tasks[k + '_triviaqa'] = kilt_tasks[k + '_triviaqa'].map(
+            add_missing_data,
+            fn_kwargs=dict(trivia_qa_subset=trivia_qa[k], triviaqa_map=triviaqa_map)
+        )
 
     return kilt_tasks
