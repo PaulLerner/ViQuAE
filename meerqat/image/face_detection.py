b5b1af2b210fbfc5b58eefbbff394080915e7986
Paul Lerner <lerner@limsi.fr>
2021-06-30 17:31:00 +0200

    fix: face_detection: save paths must be str + typo

--- meerqat/image/face_detection.py
+++ meerqat/image/face_detection.py
@@ -16,19 +16,30 @@ from datasets import load_from_disk
 from facenet_pytorch import MTCNN
 
 from meerqat.models.utils import device
-from meerqat.data.wiki import thumbnail_to_file_name, COMMONS_PATH as IMAGE_PATH
+from meerqat.data.wiki import thumbnail_to_file_name, COMMONS_PATH as IMAGE_PATH, VALID_ENCODING
 
 
 def detect_face(url, model, save_root_path=None):
     """TODO group images w.r.t. their size to allow MTCNN batch processing"""
     file_name = thumbnail_to_file_name(url)
+
+    # TODO: find new image or filter out question/article
+    encoding = file_name.split('.')[-1].lower()
+    if encoding not in VALID_ENCODING:
+        return None, None
+
     image_path = IMAGE_PATH / file_name
+    # TODO idem
+    if not image_path.exists():
+        return None, None
+
     image = Image.open(image_path).convert('RGB')
     if save_root_path:
         # if there are multiple faces, the actual save path will be
         # save_root_path/f'{image_path.stem}-{face_index}.jpg'
         # https://github.com/timesler/facenet-pytorch/blob/54c869c51e0e3e12f7f92f551cdd2ecd164e2443/models/mtcnn.py#L488
-        save_path = (save_root_path/image_path.stem).with_suffix('.jpg')
+        # HACK: make save_path str because of facenet_pytorch/models/mtcnn.py/#L468
+        save_path = str((save_root_path/image_path.stem).with_suffix('.jpg'))
     else:
         save_path = None
     face, prob = model(image, save_path=save_path, return_prob=True)
@@ -43,7 +54,7 @@ def dataset_detect_face(item, image_key='image', **kwargs):
 
 
 def dataset_detect_faces(dataset, **kwargs):
-    dataset = dataset.map(detect_face, batched=False, fn_kwargs=kwargs)
+    dataset = dataset.map(dataset_detect_face, batched=False, fn_kwargs=kwargs)
     return dataset
 
 
