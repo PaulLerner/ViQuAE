6f533d5c7cdad34884ed79733a26ebb0fa08f154
Paul Lerner <lerner@tuta.io>
2021-07-01 18:44:04 +0200

    feat: paragraph pre-processing + split into passages WIP

--- meerqat/data/loading.py
+++ meerqat/data/loading.py
@@ -36,3 +36,74 @@ def map_kilt_triviaqa():
         )
 
     return kilt_tasks
+
+
+def remove_special_fields(paragraphs):
+    """N. B. this code puts a lot of trust into KILT pre-processing
+    https://github.com/facebookresearch/KILT/blob/master/scripts/create_kilt_data_paragraphs.py
+    and simply removes the title (1st paragraph), sections titles ("Section::::") and bullet-points ("BULLET::::")
+    """
+    preprocessed_paragraphs = []
+    # drop title (first paragraph)
+    for paragraph in paragraphs[1:]:
+        # remove sections titles and bullet-points
+        if paragraph.startswith("Section::::") or paragraph.startswith("BULLET::::"):
+            continue
+        # keep as is
+        else:
+            preprocessed_paragraphs.append(paragraph)
+    return preprocessed_paragraphs
+
+
+def paragraphs_preprocess(paragraphs, method=None, **kwargs):
+    """
+    Parameters
+    ----------
+    paragraphs: List[str]
+        List of paragraphs to preprocess
+    method: str, optional
+        type of pre-processing, defaults to None (i.e. identity function)
+    **kwargs: additional arguments are passed to the appropriate pre-processing function
+
+    Returns
+    -------
+    paragraphs: List[str]
+
+    """
+    methods = {
+        None: lambda paragraphs: paragraphs,
+        "special_fields": remove_special_fields
+    }
+    return methods[method](paragraphs, **kwargs)
+
+
+def uniform_passages(paragraphs, tokenizer, n=100):
+    """
+    Parameters
+    ----------
+    paragraphs: List[str]
+        List of pre-processed paragraphs to split into passages
+
+    Returns
+    -------
+    passages: List[str]
+    """
+    text = ''.join(paragraphs)
+    raise NotImplementedError
+
+
+def make_passages(paragraphs, method=None, preprocessing_method=None, preprocessing_kwargs={}, **kwargs):
+    """
+    Parameters
+    ----------
+    paragraphs: List[str]
+        List of paragraphs to preprocess
+    method: str, optional
+        How to split the text in passages, defaults to keep the original paragraphs
+    """
+    paragraphs = paragraphs_preprocess(paragraphs, method=preprocessing_method, **preprocessing_kwargs)
+    methods = {
+        None: lambda paragraphs: paragraphs,
+        "uniform": uniform_passages
+    }
+    return methods[method](paragraphs, **kwargs)
