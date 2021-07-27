fe136d9ce33f23e1a6a872f85cc7d6465d8d57c8
Paul Lerner <lerner@tuta.io>
2021-07-27 15:45:14 +0200

    feat: face detection: group images by size to allow MTCNN batch-processing

--- meerqat/image/face_detection.py
+++ meerqat/image/face_detection.py
@@ -5,46 +5,51 @@ Options:
 --save=<root_path>                      Root path to save the detected face(s).
                                         The face will actually be saved with the same file stem as the original image.
 --disable_caching                       Disables Dataset caching (useless when using save_to_disk), see datasets.set_caching_enabled()
+--batch_size                            Batch size for Dataset.map. The actual batches processed by the model are first grouped by image size. [default: 64]
 """
 
 from docopt import docopt
 import json
 from pathlib import Path
 
-from PIL import Image
 from datasets import load_from_disk, set_caching_enabled
 
 from facenet_pytorch import MTCNN
 
 from meerqat.models.utils import device
-from meerqat.data.loading import COMMONS_PATH as IMAGE_PATH
+from meerqat.data.loading import COMMONS_PATH as IMAGE_PATH, load_image_batch
 from meerqat.data.wiki import VALID_ENCODING
 
 
-def detect_face(file_name, model, save_root_path=None):
-    """TODO group images w.r.t. their size to allow MTCNN batch processing"""
+def detect_face(file_names, model, save_root_path=None):
+    images = load_image_batch(file_names)
 
-    # TODO: find new image or filter out question/article
-    encoding = file_name.split('.')[-1].lower()
-    if encoding not in VALID_ENCODING:
-        return None, None
-
-    image_path = IMAGE_PATH / file_name
-    # TODO idem
-    if not image_path.exists():
-        return None, None
-
-    image = Image.open(image_path).convert('RGB')
-    if save_root_path:
+    # group images by size to allow MTCNN batch-processing
+    images_by_size = {}
+    for i, (image, file_name) in enumerate(zip(images, file_names)):
         # if there are multiple faces, the actual save path will be
-        # save_root_path/f'{image_path.stem}-{face_index}.jpg'
+        # save_root_path/f'{file_name}-{face_index}.jpg'
         # https://github.com/timesler/facenet-pytorch/blob/54c869c51e0e3e12f7f92f551cdd2ecd164e2443/models/mtcnn.py#L488
+
         # HACK: make save_path str because of facenet_pytorch/models/mtcnn.py/#L468
-        save_path = str((save_root_path/image_path.stem).with_suffix('.jpg'))
-    else:
-        save_path = None
-    face, prob = model(image, save_path=save_path, return_prob=True)
-    return face, prob
+        if save_root_path:
+            save_path = str((save_root_path/file_name).with_suffix('.jpg'))
+        else:
+            save_path = None
+
+        images_by_size.setdefault(image.size, dict(images=[], save_paths=[], indices=[]))
+        images_by_size[image.size]['images'].append(image)
+        images_by_size[image.size]['save_paths'].append(save_path)
+        images_by_size[image.size]['indices'].append(i)
+
+    face_batch, prob_batch = [None for _ in range(len(file_names))], [None for _ in range(len(file_names))]
+    for batch in images_by_size.values(): 
+        faces, probs = model(batch['images'], save_path=batch['save_paths'], return_prob=True)
+        for face, prob, i in zip(faces, probs, batch['indices']):
+            face_batch[i] = face
+            prob_batch[i] = prob
+
+    return face_batch, prob_batch
 
 
 def dataset_detect_face(item, image_key='image', **kwargs):
@@ -54,8 +59,8 @@ def dataset_detect_face(item, image_key='image', **kwargs):
     return item
 
 
-def dataset_detect_faces(dataset, **kwargs):
-    dataset = dataset.map(dataset_detect_face, batched=False, fn_kwargs=kwargs)
+def dataset_detect_faces(dataset, batch_size=64, **kwargs):
+    dataset = dataset.map(dataset_detect_face, batched=True, fn_kwargs=kwargs, batch_size=batch_size)
     return dataset
 
 
@@ -65,7 +70,8 @@ if __name__ == '__main__':
     dataset = load_from_disk(dataset_path)
     model_config_path = args['<model_config>']
     set_caching_enabled(not args['--disable_caching'])
-
+    batch_size = int(args['--batch_size'])
+    
     # default config
     model_config = dict(
         image_size=112,  # for arcface
