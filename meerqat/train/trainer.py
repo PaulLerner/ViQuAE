2ac521b574d09bd30e6ec487529a8e7ae18ef048
Paul Lerner <lerner@tuta.io>
2021-07-30 11:05:17 +0200

    fix: MultiPassageBERTTrainer: override Trainer evaluation loop that truncates the output

--- meerqat/train/trainer.py
+++ meerqat/train/trainer.py
@@ -4,15 +4,32 @@ import json
 from pathlib import Path
 import warnings
 from tqdm import tqdm
+import collections
+
 
 import numpy as np
 import torch
 from torch.autograd import set_detect_anomaly
+from torch.utils.data.dataset import Dataset, IterableDataset
 
 from transformers import Trainer, TrainingArguments, trainer_callback, logging
 from transformers.trainer_callback import TrainerState
-from transformers.file_utils import WEIGHTS_NAME
 from datasets import load_from_disk
+from transformers.deepspeed import deepspeed_init
+from transformers.file_utils import WEIGHTS_NAME, is_torch_tpu_available
+from transformers.trainer_pt_utils import (
+    IterableDatasetShard,
+    find_batch_size,
+    nested_concat,
+    nested_numpify
+)
+from transformers.trainer_utils import (
+    EvalLoopOutput,
+    EvalPrediction,
+    denumpify_detensorize
+)
+if is_torch_tpu_available():
+    import torch_xla.distributed.parallel_loader as pl
 
 from meerqat.data.loading import load_pretrained_in_kwargs
 
@@ -66,7 +83,7 @@ class MultiPassageBERTTrainer(MeerqatTrainer):
         self.n_relevant_passages = n_relevant_passages
         self.max_n_answers = max_n_answers
         self.eval_search_key = eval_search_key
-        default_tokenization_kwargs = dict(return_tensors='pt', padding=True, truncation=True)
+        default_tokenization_kwargs = dict(return_tensors='pt', padding='max_length', truncation=True)
         if tokenization_kwargs is None:
             tokenization_kwargs = {}
         default_tokenization_kwargs.update(tokenization_kwargs)
@@ -182,6 +199,160 @@ class MultiPassageBERTTrainer(MeerqatTrainer):
         batch = self.get_answer_position(batch, answers, answer_mask)
         return batch
 
+    def evaluation_loop(
+        self,
+        dataloader,
+        description: str,
+        prediction_loss_only: bool = None,
+        ignore_keys: list = None,
+        metric_key_prefix: str = "eval",
+    ) -> EvalLoopOutput:
+        """
+        Same as Trainer.evaluation_loop but does not truncate output to the size of the dataset because
+        there is M passages per question so the output is M times the size of the dataset
+        """
+        prediction_loss_only = (
+            prediction_loss_only if prediction_loss_only is not None else self.args.prediction_loss_only
+        )
+
+        # if eval is called w/o train init deepspeed here
+        if self.args.deepspeed and not self.deepspeed:
+
+            # XXX: eval doesn't have `resume_from_checkpoint` arg but we should be able to do eval
+            # from the checkpoint eventually
+            deepspeed_engine, _, _ = deepspeed_init(self, num_training_steps=0, resume_from_checkpoint=None)
+            self.model = deepspeed_engine.module
+            self.model_wrapped = deepspeed_engine
+            self.deepspeed = deepspeed_engine
+            # XXX: we don't need optim/sched for inference, but this needs to be sorted out, since
+            # for example the Z3-optimizer is a must for zero3 to work even for inference - what we
+            # don't need is the deepspeed basic optimizer which is self.optimizer.optimizer
+            deepspeed_engine.optimizer.optimizer = None
+            deepspeed_engine.lr_scheduler = None
+
+        model = self._wrap_model(self.model, training=False)
+
+        # if full fp16 is wanted on eval and this ``evaluation`` or ``predict`` isn't called while
+        # ``train`` is running, halve it first and then put on device
+        if not self.is_in_train and self.args.fp16_full_eval:
+            model = model.half().to(self.args.device)
+
+        batch_size = dataloader.batch_size
+
+        print(f"***** Running {description} *****")
+        if isinstance(dataloader.dataset, collections.abc.Sized):
+            print(f"  Num examples = {self.num_examples(dataloader)}")
+        else:
+            print("  Num examples: Unknown")
+        print(f"  Batch size = {batch_size}")
+
+        model.eval()
+
+        self.callback_handler.eval_dataloader = dataloader
+        # Do this before wrapping.
+        eval_dataset = dataloader.dataset
+
+        if is_torch_tpu_available():
+            dataloader = pl.ParallelLoader(dataloader, [self.args.device]).per_device_loader(self.args.device)
+
+        if self.args.past_index >= 0:
+            self._past = None
+
+        # Initialize containers
+        # losses/preds/labels on GPU/TPU (accumulated for eval_accumulation_steps)
+        losses_host = None
+        preds_host = None
+        labels_host = None
+        # losses/preds/labels on CPU (final containers)
+        all_losses = None
+        all_preds = None
+        all_labels = None
+        # Will be useful when we have an iterable dataset so don't know its length.
+
+        observed_num_examples = 0
+        # Main evaluation loop
+        for step, inputs in enumerate(dataloader):
+            # Update the observed num examples
+            observed_batch_size = find_batch_size(inputs)
+            if observed_batch_size is not None:
+                observed_num_examples += observed_batch_size
+
+            # Prediction step
+            loss, logits, labels = self.prediction_step(model, inputs, prediction_loss_only, ignore_keys=ignore_keys)
+
+            # Update containers on host
+            if loss is not None:
+                losses = self._nested_gather(loss.repeat(batch_size))
+                losses_host = losses if losses_host is None else torch.cat((losses_host, losses), dim=0)
+            if logits is not None:
+                logits = self._pad_across_processes(logits)
+                logits = self._nested_gather(logits)
+                preds_host = logits if preds_host is None else nested_concat(preds_host, logits, padding_index=-100)
+            if labels is not None:
+                labels = self._pad_across_processes(labels)
+                labels = self._nested_gather(labels)
+                labels_host = labels if labels_host is None else nested_concat(labels_host, labels, padding_index=-100)
+            self.control = self.callback_handler.on_prediction_step(self.args, self.state, self.control)
+
+            # Gather all tensors and put them back on the CPU if we have done enough accumulation steps.
+            if self.args.eval_accumulation_steps is not None and (step + 1) % self.args.eval_accumulation_steps == 0:
+                if losses_host is not None:
+                    losses = nested_numpify(losses_host)
+                    all_losses = losses if all_losses is None else np.concatenate((all_losses, losses), axis=0)
+                if preds_host is not None:
+                    logits = nested_numpify(preds_host)
+                    all_preds = logits if all_preds is None else nested_concat(all_preds, logits, padding_index=-100)
+                if labels_host is not None:
+                    labels = nested_numpify(labels_host)
+                    all_labels = (
+                        labels if all_labels is None else nested_concat(all_labels, labels, padding_index=-100)
+                    )
+
+                # Set back to None to begin a new accumulation
+                losses_host, preds_host, labels_host = None, None, None
+
+        if self.args.past_index and hasattr(self, "_past"):
+            # Clean the state at the end of the evaluation loop
+            delattr(self, "_past")
+
+        # Gather all remaining tensors and put them back on the CPU
+        if losses_host is not None:
+            losses = nested_numpify(losses_host)
+            all_losses = losses if all_losses is None else np.concatenate((all_losses, losses), axis=0)
+        if preds_host is not None:
+            logits = nested_numpify(preds_host)
+            all_preds = logits if all_preds is None else nested_concat(all_preds, logits, padding_index=-100)
+        if labels_host is not None:
+            labels = nested_numpify(labels_host)
+            all_labels = labels if all_labels is None else nested_concat(all_labels, labels, padding_index=-100)
+
+        # Number of samples
+        if not isinstance(eval_dataset, IterableDataset):
+            num_samples = len(eval_dataset)
+        elif isinstance(eval_dataset, IterableDatasetShard):
+            num_samples = eval_dataset.num_examples
+        else:
+            num_samples = observed_num_examples
+
+        # Metrics!
+        if self.compute_metrics is not None and all_preds is not None and all_labels is not None:
+            metrics = self.compute_metrics(EvalPrediction(predictions=all_preds, label_ids=all_labels))
+        else:
+            metrics = {}
+
+        # To be JSON-serializable, we need to remove numpy types or zero-d tensors
+        metrics = denumpify_detensorize(metrics)
+
+        if all_losses is not None:
+            metrics[f"{metric_key_prefix}_loss"] = all_losses.mean().item()
+
+        # Prefix all keys with metric_key_prefix + '_'
+        for key in list(metrics.keys()):
+            if not key.startswith(f"{metric_key_prefix}_"):
+                metrics[f"{metric_key_prefix}_{key}"] = metrics.pop(key)
+
+        return EvalLoopOutput(predictions=all_preds, label_ids=all_labels, metrics=metrics, num_samples=num_samples)
+
 
 def get_checkpoint(resume_from_checkpoint: str, *args, **kwargs):
     if args or kwargs:
@@ -253,7 +424,7 @@ if __name__ == "__main__":
             if not state_dict_path.exists():
                 continue
             state_dict = torch.load(state_dict_path)
-            trainer.model.load_state_dict(state_dict)
+            trainer._load_state_dict_in_model(state_dict)
 
             # optionally load trainer state for better logging
             trainer_state = resume_from_checkpoint/"trainer_state.json"
