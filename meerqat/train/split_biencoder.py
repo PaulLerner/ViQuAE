c0333a4e9d9f8afacd39e57f597216e761e1dc3a
Paul Lerner <lerner@tuta.io>
2022-04-13 11:55:46 +0200

    feat: split_biencoder: generic function to split BiEncoder in two PreTrainedModel and save them

--- meerqat/train/split_biencoder.py
+++ meerqat/train/split_biencoder.py
@@ -1,25 +1,26 @@
 """
-Usage: split_DPR.py <checkpoint_path> [--bert]
+Usage: split_biencoder.py <config> [--bert]
 
 Options:
 --bert              Save BertModel instead of DPRQuestionEncoder|DPRContextEncoder
 """
 from pathlib import Path
 from docopt import docopt
+import json
 
 import torch
-import transformers
+from transformers.file_utils import WEIGHTS_NAME
+
+from meerqat.data.loading import load_pretrained_in_kwargs
 
 
-def split_DPRBiEncoder(checkpoint_path, bert=False):
+def split_biencoder(trainee, checkpoint, bert=False):
     """Utility function to split a DPRBiEncoder in DPRQuestionEncoder and DPRContextEncoder"""
-    state_dict = torch.load(checkpoint_path/'pytorch_model.bin', map_location='cpu')
-    question_model_state_dict = {k[len('question_model.'):]: v for k, v in state_dict.items() if k.startswith('question_model.')}
-    question_model = transformers.DPRQuestionEncoder(transformers.DPRConfig())
-    question_model.load_state_dict(question_model_state_dict)
-    context_model = transformers.DPRContextEncoder(transformers.DPRConfig())
-    context_model_state_dict = {k[len('context_model.'):]: v for k, v in state_dict.items() if k.startswith('context_model.')}
-    context_model.load_state_dict(context_model_state_dict)
+    checkpoint_path = Path(checkpoint['resume_from_checkpoint'])
+    state_dict = torch.load(checkpoint_path/WEIGHTS_NAME, map_location='cpu')
+    trainee.load_state_dict(state_dict)
+    question_model = trainee.question_model
+    context_model = trainee.context_model
     if bert:
         question_model = question_model.question_encoder.bert_model
         context_model = context_model.ctx_encoder.bert_model
@@ -36,5 +37,9 @@ def split_DPRBiEncoder(checkpoint_path, bert=False):
 
 if __name__ == '__main__':
     args = docopt(__doc__)
+    config_path = Path(args['<config>'])
+    with open(config_path, "r") as file:
+        config = load_pretrained_in_kwargs(json.load(file))
+    
     bert = args['--bert']
-    split_DPRBiEncoder(Path(args['<checkpoint_path>']), bert=bert)
+    split_biencoder(**config, bert=bert)
