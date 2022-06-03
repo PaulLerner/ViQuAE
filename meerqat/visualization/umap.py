8dfe8ca659ee2de4c87cb50e49a2fd4974c6a1dd
Paul Lerner <lerner@tuta.io>
2022-06-03 16:17:57 +0200

    feat: viz: umap: color points (e.g. questions) wrt to their ranx metric

--- meerqat/visualization/umap.py
+++ meerqat/visualization/umap.py
@@ -11,9 +11,11 @@ import pandas as pd
 import umap
 
 from bokeh.plotting import figure, output_file, save
-from bokeh.models import HoverTool, ColumnDataSource
+from bokeh.models import HoverTool, ColumnDataSource, LinearColorMapper, ColorBar
 
 from datasets import load_from_disk
+
+import ranx
 
 from meerqat.data.wiki import thumbnail_to_file_name, file_name_to_thumbnail
 
@@ -33,11 +35,18 @@ def fplot(reduced_embeddings, figsize=(20,20), alpha=0.5, s=5):
 
 def iplot(reduced_embeddings, dataset, urls, input_key='input', thumb_width=128, title='UMAP projection',
           plot_width=600, plot_height=600, tools=('pan, wheel_zoom, reset'),
-          line_alpha=0.6, fill_alpha=0.6, size=4):
+          line_alpha=0.6, fill_alpha=0.6, size=4, metric=None, palette='Viridis256'):
     thumbnails = [file_name_to_thumbnail(thumbnail_to_file_name(url), thumb_width) for url in urls]
     df = pd.DataFrame(reduced_embeddings, columns=('x', 'y'))
     df['text'] = dataset[input_key]
     df['image'] = thumbnails
+    if metric is not None:
+        df[metric] = dataset[metric]
+        cmap = LinearColorMapper(palette=palette, low = df[metric].min(), high = df[metric].max())
+        fill_color = {'field': metric, 'transform': cmap}
+    else:
+        fill_color = 'gray'
+        cmap = None
     
     datasource = ColumnDataSource(df)
     
@@ -58,12 +67,24 @@ def iplot(reduced_embeddings, dataset, urls, input_key='input', thumb_width=128,
         source=datasource,
         line_alpha=line_alpha,
         fill_alpha=fill_alpha,
-        size=size
+        size=size,
+        fill_color=fill_color
     )
+    if cmap is not None:
+        cb = ColorBar(color_mapper = cmap)
+        fig.add_layout(cb, 'right')
     return fig
 
+
+def get_ranx_run(qrels_path, run_path, metric='mrr'):
+    qrels = ranx.Qrels.from_file(qrels_path, kind='trec')
+    run = ranx.Run.from_file(run_path, kind='trec')
+    ranx.evaluate(qrels, run, metrics=metric)
+    return run, metric
+
     
-def main(dataset, key, output_path, image_kb=None, shard=None, reduce_kwargs={}, fplot_kwargs={}, iplot_kwargs={}):
+def main(dataset, key, output_path, image_kb=None, shard=None, 
+         reduce_kwargs={}, fplot_kwargs={}, iplot_kwargs={}, ranx_kwargs=None):
     output_path.mkdir(exist_ok=True)
     if shard is not None:
         dataset = dataset.shuffle(seed=0).shard(shard, 0)
@@ -77,11 +98,16 @@ def main(dataset, key, output_path, image_kb=None, shard=None, reduce_kwargs={},
         print(len(urls))
     else:
         urls = dataset['url']
+    if ranx_kwargs is not None:
+        ranx_run, metric = get_ranx_run(**ranx_kwargs)   
+        dataset=dataset.map(lambda item: {metric: ranx_run.scores[metric][item["id"]]})
+    else:
+        ranx_run, metric = None, None
     embeddings = np.array(dataset[key])
     reduced_embeddings = reduce(embeddings, **reduce_kwargs)
     ffig = fplot(reduced_embeddings, **fplot_kwargs)
     ffig.savefig(output_path/f"umap_{key}.png")
-    ifig = iplot(reduced_embeddings, dataset, urls=urls, **iplot_kwargs)
+    ifig = iplot(reduced_embeddings, dataset, urls=urls, metric=metric, **iplot_kwargs)
     output_file(output_path/f"umap_{key}.html")
     save(ifig)
 
