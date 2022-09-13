8f184234e456d7363ff85d3e5fb2c6d7a2bbcd99
Paul Lerner <lerner@tuta.io>
2022-09-13 18:11:36 +0200

    docs: config: copyright should be CNRS + change theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ print(sys.path)
 # -- Project information -----------------------------------------------------
 
 project = 'meerqat'
-copyright = '2022, Paul Lerner'
+copyright = '2022, CNRS'
 author = 'Paul Lerner'
 
 # The full version, including alpha/beta/rc tags
@@ -48,7 +48,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'pydata_sphinx_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
